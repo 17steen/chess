d65c4e5f85babe333df638e4f35eacebc3bb5bf4
17steen <robinoger1207@gmail.com>
2021-02-08 21:58:13 +0100

    Changed SDL safe pointer types to use function objects, (thank you SlyBach#4412)

--- CustomDeleters.hpp
+++ CustomDeleters.hpp
@@ -2,44 +2,51 @@
 #include "SDL_render.h"
 #include <memory>
 
-using WindowPtr = std::unique_ptr<SDL_Window, decltype(&SDL_DestroyWindow)>;
-using SurfacePtr = std::unique_ptr<SDL_Surface, decltype(&SDL_FreeSurface)>;
-using TexturePtr = std::unique_ptr<SDL_Texture, decltype(&SDL_DestroyTexture)>;
-using RendererPtr =
-  std::unique_ptr<SDL_Renderer, decltype(&SDL_DestroyRenderer)>;
+struct WindowDeleter
+{
+    void operator()(SDL_Window* w) const noexcept { SDL_DestroyWindow(w); }
+};
+
+struct SurfaceDeleter
+{
+    void operator()(SDL_Surface* s) const noexcept { SDL_FreeSurface(s); }
+};
+
+struct TextureDeleter
+{
+    void operator()(SDL_Texture* t) const noexcept { SDL_DestroyTexture(t); }
+};
+
+struct RendererDeleter
+{
+    void operator()(SDL_Renderer* r) const noexcept { SDL_DestroyRenderer(r); }
+};
+
+using WindowPtr = std::unique_ptr<SDL_Window, WindowDeleter>;
+using SurfacePtr = std::unique_ptr<SDL_Surface, SurfaceDeleter>;
+using TexturePtr = std::unique_ptr<SDL_Texture, TextureDeleter>;
+using RendererPtr = std::unique_ptr<SDL_Renderer, RendererDeleter>;
 
 inline WindowPtr
 to_ptr(SDL_Window* w)
 {
-    return {
-        w,
-        &SDL_DestroyWindow,
-    };
+    return WindowPtr{ w };
 }
 
 inline SurfacePtr
 to_ptr(SDL_Surface* s)
 {
-    return {
-        s,
-        &SDL_FreeSurface,
-    };
+    return SurfacePtr{ s };
 }
 
 inline TexturePtr
 to_ptr(SDL_Texture* t)
 {
-    return {
-        t,
-        &SDL_DestroyTexture,
-    };
+    return TexturePtr{ t };
 }
 
 inline RendererPtr
 to_ptr(SDL_Renderer* r)
 {
-    return {
-        r,
-        &SDL_DestroyRenderer,
-    };
+    return RendererPtr{ r };
 }
